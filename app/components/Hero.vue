17cf85a874f069e410f4a9345d52a92602087e58
Damien <damien.h68@gmail.com>
2026-06-10 12:15:24 +0200

    Create hero

---
+++ app/components/Hero.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps({
+  description: String,
+});
+</script>
+
+<template>
+  <header id="top">
+    <div class="wrap">
+      <slot name="meta"></slot>
+
+      <div class="hero-main">
+        <slot name="headline"></slot>
+
+        <slot name="title"></slot>
+
+        <div class="hero-foot">
+          <p>
+            <span class="pr">~$</span>
+
+            {{ description }}
+          </p>
+
+          <div class="hero-cta">
+            <slot name="cta"></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <Hero :title="$t('home.title')">
+  <Hero :description="$t('home.description')">
     <template v-slot:meta>
       <div class="hero-meta">
         <span>Full-Stack Web Developer</span>
@@ -11,10 +11,26 @@
     </template>
 
     <template v-slot:headline>
-      <span class="avail"
-        ><span class="d"></span><span data-fr>Disponible — Q3 2026</span
-        ><span data-en>Available — Q3 2026</span></span
+      <span class="avail"> <span class="d"></span>{{ $t('available') }} </span>
+    </template>
+
+    <template #title>
+      <h1>
+        {{ $t('home.title_1') }}
+        <span class="amp">&amp;</span>
+        <br />
+        <span class="g">{{ $t('home.title_2') }}</span>
+        <br />
+        {{ $t('home.title_3') }}
+      </h1>
+    </template>
+
+    <template #cta>
+      <a href="#work" class="btn btn-fill">
+        {{ $t('see_projects') }}
+        →</a
       >
+      <a href="#contact" class="btn btn-out">Contact</a>
     </template>
   </Hero>
 </template>
