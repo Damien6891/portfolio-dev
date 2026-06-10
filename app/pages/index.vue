560f2df847eff73bce154867663ade92aacf0c9b
Damien <damien.h68@gmail.com>
2026-06-10 11:55:24 +0200

    create pages

---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <Hero :title="$t('home.title')">
+    <template v-slot:meta>
+      <div class="hero-meta">
+        <span>Full-Stack Web Developer</span>
+        <span>Freelance — France</span>
+        <span>~$ ./damien --hire</span>
+      </div>
+    </template>
+
+    <template v-slot:headline>
+      <span class="avail"
+        ><span class="d"></span><span data-fr>Disponible — Q3 2026</span
+        ><span data-en>Available — Q3 2026</span></span
+      >
+    </template>
+  </Hero>
+</template>
---
+++ app/pages/projects/[slug].vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <div>Un projet</div>
+</template>
