5c79b82dc99581b860e62a6770d717123b43db2a
Damien <damien.h68@gmail.com>
2026-06-11 18:08:38 +0200

    create footer

--- app/app.vue
+++ app/app.vue
@@ -2,5 +2,6 @@
   <div>
     <NavBar />
     <NuxtPage />
+    <Footer />
   </div>
 </template>
---
+++ app/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <footer class="footer">
+    <div class="wrap">
+      <div class="footer__inner">
+        <span>© 2026 Damien Hantzer</span>
+        <NuxtLink :to="$localePath('/legal')">Legal</NuxtLink>
+        <span>Full-Stack · Freelance</span>
+      </div>
+    </div>
+  </footer>
+</template>
