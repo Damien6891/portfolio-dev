f2c4d7f55bf1f0e0517f405f796eff16e6b14daf
Damien <damien.h68@gmail.com>
2026-06-11 18:07:03 +0200

    Create external Link for content

---
+++ app/components/content/ProseA.vue
@@ -0,0 +1,17 @@
+<template>
+  <a
+    v-bind="$attrs"
+    :target="isExternal ? '_blank' : undefined"
+    :rel="isExternal ? 'noopener noreferrer' : undefined"
+  >
+    <slot />
+  </a>
+</template>
+
+<script setup lang="ts">
+const attrs = useAttrs();
+
+const isExternal = computed(
+  () => typeof attrs.href === 'string' && attrs.href.startsWith('http'),
+);
+</script>
