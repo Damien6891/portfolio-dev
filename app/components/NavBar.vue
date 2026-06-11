2111f494e7eb36a82b1534b4a103d7303360094e
Damien <damien.h68@gmail.com>
2026-06-11 18:20:55 +0200

    add active to nav links

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -1,24 +1,50 @@
 <script setup>
 const { setLocale, locales, locale: currentLocal } = useI18n();
+
+const route = useRoute();
+
+const isHome = computed(() => route.path === '/' || route.path === '/en');
+
+const isActive = (hash) => {
+  return isHome.value && route.hash === hash;
+};
 </script>
 
 <template>
   <nav class="nav">
     <div class="wrap nav-inner">
-      <a class="nav__brand" href="#top">
+      <a class="nav__brand" href="/#top">
         <span class="sq"></span>damien_hantzer
       </a>
       <div class="nav__right">
         <div class="nav__links">
-          <a href="/#stack">{{ $t('nav.stack') }}</a>
-          <a href="/#services">{{ $t('nav.services') }}</a>
-          <NuxtLink to="/projects">
+          <a
+            :href="$localePath('/#stack')"
+            :class="{ active: isActive('#stack') }"
+          >
+            {{ $t('nav.stack') }}
+          </a>
+          <a
+            :href="$localePath('/#services')"
+            :class="{ active: isActive('#services') }"
+          >
+            {{ $t('nav.services') }}
+          </a>
+          <NuxtLink :to="$localePath('/projects')" active-class="active">
             {{ $t('nav.projects') }}
           </NuxtLink>
-          <a href="/#education">
+          <a
+            :href="$localePath('/#education')"
+            :class="{ active: isActive('#education') }"
+          >
             {{ $t('nav.education') }}
           </a>
-          <a href="/#contact">{{ $t('nav.contact') }}</a>
+          <a
+            :href="$localePath('/#contact')"
+            :class="{ active: isActive('#contact') }"
+          >
+            {{ $t('nav.contact') }}
+          </a>
         </div>
         <div class="nav__lang">
           <button
