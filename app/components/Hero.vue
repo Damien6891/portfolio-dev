57b355c1795f99839fb2a69f8f449e840df2491b
Damien <damien.h68@gmail.com>
2026-06-12 12:48:15 +0200

    update hero component

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -14,22 +14,23 @@ defineProps({
       <!-- <slot name="meta"></slot> -->
 
       <div class="hero__main">
-        <div v-if="$slots.headline" class="hero__headline">
-          <slot name="headline"></slot>
-        </div>
+        <div>
+          <div v-if="$slots.headline" class="hero__headline">
+            <slot name="headline"></slot>
+          </div>
 
-        <h1>
-          <slot name="title"></slot>
-        </h1>
+          <h1>
+            <slot name="title"></slot>
+          </h1>
+        </div>
 
         <div class="hero__foot">
           <p class="hero__description">
             <span class="text-accent">~$</span>
-
             {{ description }}
           </p>
 
-          <div class="hero__cta">
+          <div v-if="$slots.cta" class="hero__cta">
             <slot name="cta"></slot>
           </div>
         </div>
