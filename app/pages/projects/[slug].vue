10944fa55f16f5488aacc3cab2627d319bd6654d
Damien <damien.h68@gmail.com>
2026-06-12 16:28:54 +0200

    create slug page

--- app/pages/projects/[slug].vue
+++ app/pages/projects/[slug].vue
@@ -1,5 +1,58 @@
-<script setup></script>
+<script setup>
+const route = useRoute();
+const { locale } = useI18n();
+const { data: project } = await useAsyncData(
+  `project-${locale.value}-${route.params.slug}`,
+  () => {
+    return queryCollection(`projects_${locale.value}`)
+      .path(`/projects/${route.params.slug}`)
+      .first();
+  },
+);
+</script>
 
 <template>
-  <div>Un projet</div>
+  <div>
+    <Hero :description="project.projectDescription">
+      <template v-slot:meta>
+        <span>
+          ~$ ./ projects /
+          <span class="text-accent">{{ project.type }}</span>
+        </span>
+        <span>{{ 2025 }}</span>
+      </template>
+
+      <template #headline>
+        <span class="square"></span>{{ $t('projects.study_case') }} -
+        {{ project.type }}
+      </template>
+
+      <template #title>
+        <span v-html="project.projectTitle"></span>
+      </template>
+
+      <template #cta>
+        <div class="table">
+          <div class="table__row">
+            <span class="label">{{ $t('role') }}</span>
+            <span class="value">{{ project.role }}</span>
+          </div>
+          <div class="table__row">
+            <span class="label">{{ $t('year') }}</span>
+            <span class="value">{{ project.year }}</span>
+          </div>
+          <div class="table__row">
+            <span class="label">{{ $t('duration') }}</span>
+            <span class="value">{{ project.duration }}</span>
+          </div>
+          <div class="table__row">
+            <span class="label">{{ $t('status') }}</span>
+            <span class="value status">{{ project.status }}</span>
+          </div>
+        </div>
+      </template>
+    </Hero>
+
+    <ContentRenderer :value="project" class="prose" />
+  </div>
 </template>
