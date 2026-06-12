8e7f263a5c582f0ecfd2bead8dda9dcb5c1ffcfd
Damien <damien.h68@gmail.com>
2026-06-12 12:46:59 +0200

    create card component

---
+++ app/components/Card.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string;
+  description?: string;
+  tags?: Array<String>;
+  slug?: any;
+  imagePath?: any;
+  gridClasses?: String;
+  full?: boolean;
+}>();
+</script>
+
+<template>
+  <NuxtLink :class="[gridClasses, 'card', { 'card--full': full }]" :to="slug">
+    <div class="card__image">
+      <NuxtImg :src="imagePath" />
+    </div>
+    <div class="card__body">
+      <h3 class="card__title">{{ title }}</h3>
+      <p class="card__description">
+        {{ description }}
+      </p>
+
+      <div class="card__tags">
+        <span v-for="(tag, index) in tags" :key="index"> {{ tag }} </span>
+      </div>
+    </div>
+  </NuxtLink>
+</template>
