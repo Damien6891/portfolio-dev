465659a9df18b1bba2cb45829a2fba47ecd9b3d9
Damien <damien.h68@gmail.com>
2026-06-12 16:29:14 +0200

    create section headline component

---
+++ app/components/content/SectionHeadline.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps<{
+  number?: String;
+  title?: string;
+}>();
+</script>
+
+<template>
+  <div class="section__headline">
+    <b>{{ number }}</b
+    >/
+    {{ title }}
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/scss/abstracts/' as *;
+
+.section__headline {
+  padding: 16px 26px;
+  max-width: $wrapper-width;
+  margin-inline: auto;
+  width: 100%;
+
+  &.border-top {
+    border-top: 1px solid $color-border;
+  }
+}
+</style>
