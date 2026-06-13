ea70b2d2c573dbc7cdc719ff1cc50a59bd2e5c69
Damien <damien.h68@gmail.com>
2026-06-13 14:10:21 +0200

    create cta in slug page

--- app/pages/projects/[slug].vue
+++ app/pages/projects/[slug].vue
@@ -33,7 +33,7 @@ const { data: project } = await useAsyncData(
 
       <template #links>
         <NuxtLink :href="project.url" class="btn btn--filled" target="_blank">
-          Voir le site
+          {{ $t('see_website') }}
         </NuxtLink>
       </template>
 
@@ -60,5 +60,55 @@ const { data: project } = await useAsyncData(
     </Hero>
 
     <ContentRenderer :value="project" class="prose" />
+
+    <section class="nextbar wrap">
+      <div class="nextbar__title">
+        <span>~$ ./next</span>
+        <p v-html="$t('similar_project')"></p>
+      </div>
+      <div class="nextbar__links">
+        <NuxtLink :href="$localePath('/#contact')" class="btn btn--filled">
+          {{ $t('contact.contact_me') }}
+        </NuxtLink>
+        <NuxtLink :href="$localePath('projects')" class="btn btn--outline">
+          {{ $t('other_projects') }}
+        </NuxtLink>
+      </div>
+    </section>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@use '../../assets/scss/abstracts/' as *;
+
+.nextbar {
+  padding-block: 50px;
+  display: flex;
+  justify-content: space-between;
+
+  @include tablet {
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  &__title {
+    text-transform: uppercase;
+    span {
+      color: $color-text-disabled;
+    }
+
+    p {
+      font-size: clamp(24px, 4vw, 44px);
+      font-weight: 800;
+      letter-spacing: -0.03em;
+      line-height: 1;
+    }
+  }
+
+  &__links {
+    display: flex;
+    gap: 11px;
+    flex-wrap: wrap;
+  }
+}
+</style>
